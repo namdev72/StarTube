1fc02e365ca12af2476e4d5aebbedd8d073e3bb6
namdev72 <namdevkartik72@gmail.com>
2026-06-06 09:55:13 +0530

    add user channel profile and watch history retrieval features

---
+++ src/models/subscription.model.js
@@ -0,0 +1,18 @@
+import mongoose , {Schema} from "mongoose";
+
+const subscriptionSchema=new Schema({
+    subscriber:{
+        //one to subscribing
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+    channel:{
+        //one to whom subscribed
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{
+    timestamps:true
+})
+
+export const Subscription=mongoose.model("Subscription",subscriptionSchema)
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,6 @@
 
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser ,refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser ,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import { upload} from "../middlewares/multer.middleware.js";
 import {vertifyJWT} from "../middlewares/auth.middleware.js"
 
@@ -27,4 +27,12 @@ router.route("refresh-token").post(refreshAccessToken)
 //we will add middle to verify the jwt  before logout
 //verify user ke thourgh we added the user in req , so we have the access of user for logout
 router.route("/logout").post(vertifyJWT, logoutUser)
+router.route("/change-password").post(vertifyJWT,changeCurrentPassword)
+router.route("/current-user").get(vertifyJWT,getCurrentUser)
+router.route("/update-account").patch(vertifyJWT,updateAccountDetails)
+router.route("/avatar").patch(vertifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(vertifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(vertifyJWT,getUserChannelProfile)
+router.route("/history").get(vertifyJWT,getWatchHistory)
+
 export default router
